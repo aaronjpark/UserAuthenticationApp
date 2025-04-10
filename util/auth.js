8bdf403abebb67581882cb4d28d5ba63baa0ce78
Aaron Park <aaronj.park0304@gmail.com>
2025-04-10 05:59:50 -0500

    Refactor authentication logic and enhance login screen functionality

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyAwABYmM9c9jbRJKjN3RHc23cqErL9UiMI";
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+  return response;
+}
+
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
+}
+
+export function login(email, password) {
+  return authenticate("logIn", email, password);
 }
